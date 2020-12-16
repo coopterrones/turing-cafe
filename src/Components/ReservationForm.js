c9b538a8bc82b68608a78a170d9849e9b6406764
Cooper Terrones <cooper.terrones@outlook.com>
2020-12-16 10:02:51 -0700

    Form styling to match mock

--- src/Components/ReservationForm.js
+++ src/Components/ReservationForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/ReservationForm.css';
 
 class ReservationForm extends React.Component {
   constructor(props) {
@@ -32,36 +33,36 @@ class ReservationForm extends React.Component {
 
   render() {
     return(
-      <form>
-        <input
+      <form className="reservation-form">
+        <input className="reservation-form-input"
           type='text'
           placeholder='Name'
           name='name'
           value={this.state.name}
           onChange={event => this.handleChange(event)}
         />
-        <input
+        <input className="reservation-form-input"
           type='text'
           placeholder='Date'
           name='date'
           value={this.state.Date}
           onChange={event => this.handleChange(event)}
         />
-          <input
+          <input className="reservation-form-input"
             type='text'
             placeholder='Time'
             name='time'
             value={this.state.time}
             onChange={event => this.handleChange(event)}
           />
-        <input
+        <input className="reservation-form-input"
           type='text'
           placeholder='Number Of Guests'
           name='number'
           value={this.state.number}
           onChange={event => this.handleChange(event)}
         />
-        <button onClick={event => this.submitReservation(event)}>Make Reservation</button>
+        <button className="reservation-form-button" onClick={event => this.submitReservation(event)}>Make Reservation</button>
       </form>      
     )
   }
