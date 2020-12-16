d873e4d3bd050af543a65cee2ca0b40ab5dccf1a
Cooper Terrones <cooper.terrones@outlook.com>
2020-12-16 09:48:42 -0700

    Form values viewable live -- handleChange and state within ReservationForm are viewable -- need to lift state to App on submit

--- src/Components/ReservationForm.js
+++ src/Components/ReservationForm.js
@@ -12,6 +12,10 @@ class ReservationForm extends React.Component {
     };
   }
 
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
   render() {
     return(
       <form>
@@ -20,24 +24,28 @@ class ReservationForm extends React.Component {
           placeholder='Name'
           name='name'
           value={this.state.name}
+          onChange={event => this.handleChange(event)}
         />
         <input
           type='text'
           placeholder='Date'
           name='date'
           value={this.state.Date}
+          onChange={event => this.handleChange(event)}
         />
           <input
             type='text'
             placeholder='Time'
             name='time'
             value={this.state.time}
+            onChange={event => this.handleChange(event)}
           />
         <input
           type='text'
           placeholder='Number Of Guests'
           name='number'
           value={this.state.number}
+          onChange={event => this.handleChange(event)}
         />
         <button>Make Reservation</button>
       </form>      
